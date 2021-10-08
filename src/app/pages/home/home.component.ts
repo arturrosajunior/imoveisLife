6a18833192b8a149a9b118ec0e90464549cb6a31
Artur Rosa <arturaparecidorosa@gmail.com>
2021-10-08 14:42:03 -0300

    home e list imoveis

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -10,27 +10,4 @@ export class HomeComponent implements OnInit {
   constructor(public imoveisService: ImoveisService) {}
 
   ngOnInit(): void {}
-
-  myFunction() {
-    var input, filter, ul, li, a, i, txtValue;
-    input = document.getElementById('myInput');
-
-    filter = input.value.toUpperCase();
-
-    ul = document.getElementById('myUL');
-
-    li = ul.getElementsByTagName('li');
-
-    for (i = 0; i < li.length; i++) {
-      a = li[i].getElementsByTagName('label')[0];
-
-      txtValue = a.innerText;
-
-      if (txtValue.toUpperCase().indexOf(filter) > -1) {
-        li[i].style.display = '';
-      } else {
-        li[i].style.display = 'none';
-      }
-    }
-  }
 }
